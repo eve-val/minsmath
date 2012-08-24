58bb2f3cf6dec671f29cd45781dd0f55f9daf376
aliaras <aliarasiii@gmail.com>
2012-08-23 18:03:35 -0700

    Improved load times, bugfixes. The minerals calculator now pulls from a database.

---
+++ database.py
@@ -0,0 +1,61 @@
+import sqlite3
+import yaml
+
+def db_setup():
+    conn = sqlite3.connect('eve.db')
+    c = conn.cursor()
+
+    #these are all the relevant fields from invTypes; if more are required they can be added
+    c.execute('CREATE TABLE inv_types (type_id int, group_id int, type_name text)') 
+    temp = load_yaml('invTypes.yaml')['data']
+    ins = [(x['typeID'],x['groupID'],x['typeName']) for x in temp]
+    c.executemany('INSERT INTO inv_types VALUES (?,?,?)', ins)
+    
+    #solar systems table
+    c.execute('CREATE TABLE solar_systems (region_id int, system_id int, system_name text)')
+    temp = load_yaml('mapSolarSystems.yaml')['data']
+    ins = [(x['regionID'],x['solarSystemID'],x['solarSystemName']) for x in temp]
+    c.executemany('INSERT INTO solar_systems VALUES (?,?,?)', ins)
+    
+    #high sec stations table: can get refinery efficiency here, which means could compute from char sheet
+    c.execute('CREATE TABLE stations (id int, system_id int, region_id int, name text)')
+    temp = load_yaml('staStations.yaml')['data']
+    ins = [(x['stationID'],x['solarSystemID'],x['regionID'],x['stationName']) for x in temp]
+    c.executemany('INSERT INTO stations VALUES (?,?,?,?)', ins)
+    
+    #refining results table
+    c.execute('CREATE TABLE item_materials (type_id int, material_id int, quantity int)')
+    temp = load_yaml('invTypeMaterials.yaml')['data']
+    ins = [(x['typeID'],x['materialTypeID'],x['quantity']) for x in temp]
+    c.executemany('INSERT INTO item_materials VALUES (?,?,?)', ins)
+
+    conn.commit()
+    
+    
+def load_yaml(filename):
+    f = open(filename)
+    return yaml.load(f)
+
+class database(object):
+
+    def __init__(self):
+    
+        self.conn = sqlite3.connect('eve.db')
+        self.c = self.conn.cursor()
+
+    def add_temp_table(self,table,fields):
+        self.c.execute('CREATE TABLE '+table+' '+str(fields))
+
+    def add_temp_values(self,table,pattern,data):
+        self.c.executemany('INSERT INTO '+table+' VALUES '+pattern,data)
+
+    def query(self,query,inputs):
+
+        '''A wrapper for database queries. Expects an SQL query, with ?s for inputs that require sanitization. inputs is a tuple containing the inputs that should replace the ?s. Returns a list of all matching rows.'''
+
+        self.c.execute(query,inputs)
+        return self.c.fetchall()
+
+if __name__ == '__main__':
+
+    db_setup()
--- minerals_calculator.py
+++ minerals_calculator.py
@@ -1,9 +1,8 @@
 import evelink.api
 import evelink.eve
 import evelink.char
-import yaml
-from yaml import load
 import evecentral
+from database import database
 
 KEY_ERROR = -1
 
@@ -15,10 +14,10 @@ class minerals_calculator(object):
             keyid, vcode, charname = charinfo.read().split(',')
             keyid = int(keyid)
         except IOError:
-            keyid, vcode, charname = get_charinfo()
+            keyid, vcode, charname = self.get_charinfo()
         self.get_character(charname,(keyid,vcode))
+        self.database = database()
         self.get_tables()
-
 
 
     def get_character(self, charname, apikey):
@@ -28,29 +27,27 @@ class minerals_calculator(object):
         self.char = evelink.char.Char(char_id = charid, api= self.api)
         self.assets = self.char.assets()
 
+    def get_charinfo(self):
+        keyid = int(raw_input('What is your api key id? \n'))
+        vcode = raw_input('What is your api vcode? \n')
+        charname = raw_input('What is your character name? \n')
+        out = file('char_info.txt','w')
+        out.write(str(keyid) + ',' + vcode + ',' + charname)
+        out.close()
+        return [keyid,vcode,charname]
+
+
     def refresh_assets(self):
         self.assets = self.char.assets()
 
     def get_tables(self):
-        print('please wait. This program will now suck ALL the memory.')
-        ins = self.load_yaml('invTypes.yaml')
-        self.item_types = ins['data']
-        print('load 1/5 complete.')
-        ins = self.load_yaml('mapSolarSystems.yaml')
-        self.solar_systems = ins['data']
-        print('load 2/5 complete.')
-        ins = self.load_yaml('invTypeMaterials.yaml')
-        self.item_materials = ins['data'] 
-        print('load 3/5 complete.')
-        ins = self.load_yaml('staStations.yaml')
-        self.highsec_stations = ins['data']
-        print('load 4/5 complete.')
         self.nullsec_stations = self.eve.conquerable_stations()
+        inserts = []
+        for key in self.nullsec_stations:
+            temp = self.nullsec_stations[key]
+            inserts.append((temp['id'],temp['system_id'],self.get_value('region_id','solar_systems','system_id',temp['system_id'])[0],temp['name']))
+        self.database.add_temp_values('stations','(?,?,?,?)',inserts)
         print('loading complete.')
-
-    def load_yaml(self,filename):
-        f = open(filename)
-        return(yaml.load(f))    
 
     def get_charinfo(self):
         keyid = int(raw_input('What is your api key id? \n'))
@@ -59,52 +56,27 @@ class minerals_calculator(object):
         out = file('char_info.txt','w')
         out.write(str(keyid) + ',' + vcode + ',' + charname)
         out.close()
-        return([keyid,vcode,charname])
+        return [keyid,vcode,charname]
 
-    def get_itemid_by_name(self,name):
-        for item in self.item_types:
-            if(item['typeName'] == name):
-                return(item['typeID'])
-        print('Name not found')
-
-    def get_sysid_by_name(self,name):
-        for elem in self.solar_systems:
-            if elem['solarSystemName'] == name:
-                return(elem['solarSystemID'])
-
-    def get_regionid_by_sysname(self,sysname):
-        for elem in self.solar_systems:
-            if elem['solarSystemName'] == sysname:
-                return(elem['regionID'])
-
-    def get_staids_by_sysid(self,sysid):
-        for key in self.nullsec_stations:
-            if self.nullsec_stations[key]['system_id'] == sysid:
-                return([key])
-        results = []
-        for elem in self.highsec_stations:
-            if elem['solarSystemID'] == sysid:
-                results.append(elem['stationID'])
-        return(results)
-
-    def get_staname_by_staid(self, staid):
-        for key in self.nullsec_stations:
-            if key == staid:
-                return(self.nullsec_stations[key]['name'])
-        for elem in self.highsec_stations:
-            if elem['stationID'] == staid:
-                return(elem['stationName'])
+    def get_value(self,field,table,key,value):
+        result = self.database.query('select '+field+' from '+table+' where '+key+'=?',(value,))
+        if len(result) == 0:
+            return result
+        #database returns a tuple. If the tuple has only one element, flatten it.
+        elif len(result[0]) == 1:
+            result = [x[0] for x in result]
+        return result
 
     def get_assets_at_station(self,name):
-        sysid = self.get_sysid_by_name(name)
-        staids = self.get_staids_by_sysid(sysid)
-        system_assets = []
-        asset_ids = []
+        sysid = self.get_value('system_id','solar_systems','system_name',name)[0]
+        staids = self.get_value('id','stations','system_id',sysid)
+        system_assets = [] #list of assets at stations.
+        asset_ids = [] #ids of stations with assets at them
         for station in staids:
             try:
                 system_assets.append(self.assets[station]['contents'])
                 asset_ids.append(station)
-            except KeyError:
+            except KeyError: #KeyError means no assets were found at that station.
                 pass
         if len(system_assets) == 0:
             print('you appear not to have anything there!')
@@ -112,9 +84,11 @@ class minerals_calculator(object):
         elif len(system_assets) == 1:
             return(system_assets[0])
         else:
-            for sta in assets_ids:
-                print('[' + str(iter) + '] \t' + self.get_staname_by_staid(sta))
-            result = raw_input('Which station did you mean?')
+            i = 0
+            for sta in asset_ids:
+                print('[' + str(i) + '] \t' + self.get_value('name','stations','id',sta)[0])
+                i = i+1
+            result = int(raw_input('Which station did you mean?'))
             return(system_assets[result])
 
     def offer_containers(self,assets):
@@ -140,24 +114,11 @@ class minerals_calculator(object):
             print('Here are the available containers:')
             print('[0]\tHangar')
             for key in containers:
-                print('['+str(key)+']' + '\t' + self.get_item_name_by_type(containers[key]['item_type_id']))
+                print('['+str(key)+']' + '\t' + self.get_value('type_name','inv_types','type_id',containers[key]['item_type_id'])[0])
             selection = int(raw_input('Which container would you like to look in? \n'))
             if(selection != 0):
                 refine_assets = containers[selection]['contents']
         return(refine_assets)
-
-    def get_repro_results(self,itemid):
-        results = []
-        for item in self.item_materials:
-            if item['typeID'] == itemid:
-                results.append((item['materialTypeID'],int(item['quantity'])))
-        return(results)
-
-    def get_item_name_by_type(self,typeid):
-        for item in self.item_types:
-            if item['typeID'] == typeid:
-                return(item['typeName'])
-        print('Item ID not found')
 
     def getprices(self):
         ''' reads in prices from a text file. TODO: replace w/eve-central'''
@@ -166,14 +127,14 @@ class minerals_calculator(object):
         pricesraw = []
         for line in raw:
             foo = line.split(',')
-            pricesraw.append((self.get_itemid_by_name(foo[0]),float(foo[1])))
+            pricesraw.append((self.get_value('type_id','inv_types','type_name',foo[0])[0],float(foo[1])))
         prices = dict(pricesraw)
         return(prices)
      
-    def print_refine(self,refine_assets):
+    def print_refine(self,refine_assets,region):
         res = []
         for item in refine_assets:
-            repro = self.get_repro_results(item['item_type_id'])
+            repro = self.get_value('material_id,quantity','item_materials','type_id',item['item_type_id'])
             refine_price = addm(repro,prices,refinery*0.01,standings*0.01)*item['quantity']
             sell_price = evecentral.find_best_price(item['item_type_id'],region)*item['quantity']
             if(refine_price > sell_price):
@@ -182,7 +143,7 @@ class minerals_calculator(object):
             else:
                 verdict = "sell"
                 delta = sell_price - refine_price
-            res.append(str(self.get_item_name_by_type(item['item_type_id'])) + '\t' + verdict + '\t' + str(delta))
+            res.append(str(self.get_value('type_name','inv_types','type_id',item['item_type_id'])[0]) + '\t' + verdict + '\t Refine price per: '+str(refine_price/item['quantity']) +'\t Difference: ' + str(delta))
         res.sort()
         for item in res:
             print(item)
@@ -200,10 +161,10 @@ if __name__ == '__main__':
     while True:
         prices = calc.getprices()
         location = raw_input('Which system are your assets in? Please type the full name. \n')
-        region = calc.get_regionid_by_sysname(location)
+        region = calc.get_value('region_id','solar_systems','system_name',location)[0]
         calc.refresh_assets()
         refinery = float(raw_input('What is your net refining yield in percent? \n'))
         standings = float(raw_input('And what is the refinery tax in percent? \n'))
         refine_assets = calc.get_refine_list(location)
         if(refine_assets != KEY_ERROR):
-            calc.print_refine(refine_assets)
+            calc.print_refine(refine_assets,region)
