c5665873ea2ba2e8d1eaebab5c12b4b8fdedc3d4
aliaras <aliarasiii@gmail.com>
2012-08-27 21:26:27 -0700

    Fixed errors caused by un-market-able items

--- evecentral.py
+++ evecentral.py
@@ -2,14 +2,19 @@ import urllib2
 from xml.etree import ElementTree
 
 def get_buys(item_id,region):
-    response = urllib2.urlopen("http://api.eve-central.com/api/quicklook?typeid="+str(item_id)+"&regionlimit="+str(region))
-    tree = ElementTree.parse(response)
-    ql = tree.find('quicklook')
-    buy_orders = ql.find('buy_orders')
+    try:
+        response = urllib2.urlopen("http://api.eve-central.com/api/quicklook?typeid="+str(item_id)+"&regionlimit="+str(region))
+        tree = ElementTree.parse(response)
+        ql = tree.find('quicklook')
+        buy_orders = ql.find('buy_orders')
+    except urllib2.HTTPError:
+        return(-1)
     return(buy_orders)
 
 def find_best_price(item_id,region):
     buy_orders = get_buys(item_id,region)
+    if buy_orders == -1:
+        return 0
     best = 0
     for order in buy_orders:
        if float(order.find('price').text) > best:
